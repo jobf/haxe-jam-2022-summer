f44932238e34c04291dd9c2a4a7e3f24d457e03d
jf <j@b.f>
2022-05-10 22:54:37 +0100

    CountDown helper

--- src/tyke/Loop.hx
+++ src/tyke/Loop.hx
@@ -140,3 +140,25 @@ class PhysicalStageLoop extends PeoteViewLoop
 	}
 }
 
+class CountDown{
+	var duration:Float;
+	var countDown:Float;
+	var onComplete:() -> Void;
+	var restartWhenComplete:Bool;
+	public function new(durationSeconds:Float, onComplete:Void->Void, restartWhenComplete:Bool = false){
+		this.duration = durationSeconds;
+		this.onComplete = onComplete;
+		this.restartWhenComplete = restartWhenComplete;
+		this.countDown = durationSeconds;
+	}
+	public function update(elapsedSeconds:Float){
+		countDown -= elapsedSeconds;
+		if(countDown <= 0){
+			onComplete();
+			if(restartWhenComplete){
+				countDown = duration;
+			}
+		}
+	}
+
+}
