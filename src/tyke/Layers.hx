38b0d99caf5fb3a397ec32dd2846f4eed03bc498
jf <j@b.f>
2022-05-10 22:48:58 +0100

    GlyphLayer mouse point to grid point

--- src/tyke/Layers.hx
+++ src/tyke/Layers.hx
@@ -29,22 +29,24 @@ class GridLayer<T> extends GridStructure<T> {
 	}
 }
 
-
-
 class GlyphLayer extends GridLayer<GlyphModel> {
 	var fontProgram:FontProgram<FontStyle>;
 	var fontStyle:FontStyle;
+
 	public var offsetX:Int;
 	public var offsetY:Int;
 	public var cellWidth(get, null):Float;
+
 	function get_cellWidth():Float {
 		return fontStyle == null ? 20 : fontStyle.width;
 	}
+
 	public var cellHeight(get, null):Float;
+
 	function get_cellHeight():Float {
 		return fontStyle == null ? 36 : fontStyle.height;
 	}
-	
+
 	public function new(config:GlyphLayerConfig, fontProgram:FontProgram<FontStyle>) {
 		this.fontProgram = fontProgram;
 		fontProgram.fontStyle.bgColor = 0x00000000;
@@ -67,10 +69,11 @@ class GlyphLayer extends GridLayer<GlyphModel> {
 	}
 
 	public function draw() {
+		// trace('draw');
 		for (i => cell in cells) {
 			cell.glyph.color = palette.colorOrDefault(cell.paletteIndexFg);
 			var bgColor = palette.colorOrDefault(cell.paletteIndexBg);
-			if(cell.paletteIndexBg > 0){
+			if (cell.paletteIndexBg > 0) {
 				var alpha = Math.ceil(255 * cell.bgIntensity);
 				cell.glyph.bgColor = bgColor.changeAlpha(alpha);
 			}
@@ -78,8 +81,16 @@ class GlyphLayer extends GridLayer<GlyphModel> {
 			fontProgram.glyphSetChar(cell.glyph, cell.char == 0x0 ? " ".charCodeAt(0) : cell.char);
 		}
 	}
-	
-}
+
+	public function screenToGrid(pixelX:Float, pixelY:Float, pixelW:Float, pixelH:Float):Point {
+		var pX = pixelX / pixelW;
+		var pY = pixelY / pixelH;
+		var numVisibleColumns = pixelW / fontProgram.fontStyle.width;
+		var numVisibleRows = pixelH / fontProgram.fontStyle.height;
+		var c = Math.floor(pX * numVisibleColumns);
+		var r = Math.floor(pY * numVisibleRows);
+		return {x: c, y: r};
+	}
 
 
 typedef GlyphLayerConfig = LayerConfig<GlyphModel>
